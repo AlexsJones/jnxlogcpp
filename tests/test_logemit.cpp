6fb57d7f109d01a8e6b25ca40d88f67c0ecf27ac
AlexsJones <tibbar@freedommail.ch>
2016-01-24 21:08:38 +0000

    updated

--- tests/test_logemit.cpp
+++ tests/test_logemit.cpp
@@ -2,7 +2,7 @@
  *     File Name           :     test/test_example.c
  *     Created By          :     anon
  *     Creation Date       :     [2015-12-17 13:15]
- *     Last Modified       :     [2016-01-24 20:52]
+ *     Last Modified       :     [2016-01-24 21:08]
  *     Description         :
  **********************************************************************************/
 #include <assert.h>
@@ -10,8 +10,10 @@
 #include "ioappender.hpp"
 #include "configuration.hpp"
 #include "logger.hpp"
+
+#ifndef __clang__
 #include <chrono>
-#include <thread>
+#endif
 typedef std::chrono::high_resolution_clock Clock;
 using namespace std;
 
@@ -23,14 +25,18 @@ void test_log_emit_sync()
   Configuration c( { &appender } );
 
   Logger l(c);
+#ifndef __clang__
   auto start = Clock::now();
+#endif
   for(int x = 0; x < 10; ++x) {
     l.Write(INFO,__FILE__,__FUNCTION__,__LINE__,"Count %d",x);
   }
+#ifndef __clang__
   auto update = Clock::now();
   std::cout << "Emit took: " 
     << std::chrono::duration_cast<std::chrono::nanoseconds>(update - start).count()
     << " nanoseconds" << std::endl;
+#endif
 }
 int main(int argc, char** argv)
 {
